ff190f6cd6a43e50251d35d0e5dc1475cdd431ef
linzj <zuojian.lzj@alibaba-inc.com>
2017-02-27 18:57:05 +0800

    * test rnn

--- testrnn.py
+++ testrnn.py
@@ -1,9 +1,6 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import numpy as np
 import tensorflow as tf
+
 
 # data I/O
 data = open('input.txt', 'r').read() # should be simple plain text file
@@ -19,26 +16,55 @@ seq_length = 25 # number of steps to unroll the RNN for
 learning_rate = 1e-1
 
 graph = tf.Graph()
-def build_graph():
+def build_graph(keep_prob = 1.0):
     with graph.as_default():
         init = tf.global_variables_initializer()
-        inputs = tf.placeholder(tf.int32, shape=[16])
-        ids = tf.placeholder(tf.int32, shape=[2, 2])
-        embeddings = tf.reshape(inputs, [4, 4])
-        return init, inputs, ids, tf.nn.embedding_lookup(embeddings, ids)
+        _inputs = tf.placeholder(tf.int32, shape=(seq_length))
+        targets = tf.placeholder(tf.int32, shape=(seq_length))
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable(
+                "embedding", [vocab_size, hidden_size], dtype=tf.float32)
+            inputs = tf.nn.embedding_lookup(embedding, _inputs)
+        cell = tf.contrib.rnn.BasicLSTMCell(hidden_size)
+        if keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, keep_prob)
+
+        state = cell.zero_state(seq_length, tf.float32)
+        
+        with tf.variable_scope("RNN"):
+            for time_step in range(seq_length):
+                if time_step > 0: tf.get_variable_scope().reuse_variables()
+                (cell_output, state) = cell(inputs[time_step], state)
+                outputs.append(cell_output)
+
+        output = tf.reshape(tf.concat(outputs, 1), [-1, size])
+        softmax_w = tf.get_variable(
+            "softmax_w", [size, vocab_size], dtype=tf.float32)
+        softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=tf.float32)
+        logits = tf.matmul(output, softmax_w) + softmax_b
+        losses = tf.nn.softmax_cross_entropy_with_logits(labels=targets, logits=logits)
+        loss = tf.reduce_sum(losses)
+        train_op = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
+        return init, train_op, loss, _inputs, targets
+        
 
 def main():
-    init, inputs, ids, result = build_graph()
+    init, train_op, loss, inputs, targets  = build_graph()
+    n = 0
+    p = 0
     with tf.Session(graph=graph) as session:
       # We must initialize all variables before we use them.
         init.run()
         print("Initialized")
-        result_val = session.run([result], feed_dict = {inputs: [1, 2, 3, 4, 
-                5, 6, 7, 8,
-                9, 10, 11, 12,
-                13, 14, 15, 16],
-                ids: [[1, 1], [2, 2]]})
-        print("result: " + str(result_val))
+        if p+seq_length+1 >= len(data) or n == 0: 
+            p = 0 # go from start of data
+        inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+        targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+        while True:
+            _, loss = session.run([train_op, loss], feed_dict = {inputs: inputs,
+                    targets: targets})
+            if n % 100 == 0:
+                print 'iter %d, loss: %f' % (n, loss) # print progress
 
 if __name__ == '__main__':
     main()
