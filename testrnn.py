c947d52637935bd0b3db364466a38eaf1e07c470
linzj <manjian2006@gmail.com>
2017-02-27 23:08:19 +0800

    RNN works.

--- testrnn.py
+++ testrnn.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import tensorflow as tf
 
 
@@ -13,58 +14,63 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 # hyperparameters
 hidden_size = 100 # size of hidden layer of neurons
 seq_length = 25 # number of steps to unroll the RNN for
+batch_size = data_size / seq_length
 learning_rate = 1e-1
 
 graph = tf.Graph()
 def build_graph(keep_prob = 1.0):
     with graph.as_default():
-        init = tf.global_variables_initializer()
-        _inputs = tf.placeholder(tf.int32, shape=(seq_length))
-        targets = tf.placeholder(tf.int32, shape=(seq_length))
+        r_inputs = _inputs = tf.placeholder(tf.int32, shape=seq_length * batch_size)
+        _inputs = tf.reshape(_inputs, (batch_size, seq_length))
+        r_targets = _targets = tf.placeholder(tf.int32, shape=seq_length * batch_size)
+        _targets = tf.reshape(_targets, (batch_size, seq_length))
+        _targets = tf.one_hot(_targets, vocab_size)
         with tf.device("/cpu:0"):
             embedding = tf.get_variable(
-                "embedding", [vocab_size, hidden_size], dtype=tf.float32)
+                "embedding", [vocab_size, hidden_size], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev = 1 / math.sqrt(hidden_size)))
             inputs = tf.nn.embedding_lookup(embedding, _inputs)
         cell = tf.contrib.rnn.BasicLSTMCell(hidden_size)
         if keep_prob < 1:
             inputs = tf.nn.dropout(inputs, keep_prob)
 
-        state = cell.zero_state(seq_length, tf.float32)
+        state = cell.zero_state(batch_size, tf.float32)
         
+        outputs = []
         with tf.variable_scope("RNN"):
             for time_step in range(seq_length):
                 if time_step > 0: tf.get_variable_scope().reuse_variables()
-                (cell_output, state) = cell(inputs[time_step], state)
+                (cell_output, state) = cell(inputs[:,time_step,:], state)
                 outputs.append(cell_output)
 
-        output = tf.reshape(tf.concat(outputs, 1), [-1, size])
+        output = tf.reshape(tf.concat(outputs, 1), [-1, hidden_size])
+        print(output.shape)
         softmax_w = tf.get_variable(
-            "softmax_w", [size, vocab_size], dtype=tf.float32)
+            "softmax_w", [hidden_size, vocab_size], dtype=tf.float32)
         softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=tf.float32)
         logits = tf.matmul(output, softmax_w) + softmax_b
-        losses = tf.nn.softmax_cross_entropy_with_logits(labels=targets, logits=logits)
+        losses = tf.nn.softmax_cross_entropy_with_logits(labels=_targets, logits=logits)
         loss = tf.reduce_sum(losses)
         train_op = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
-        return init, train_op, loss, _inputs, targets
+        return train_op, loss, r_inputs, r_targets
         
 
 def main():
-    init, train_op, loss, inputs, targets  = build_graph()
+    train_op, _loss, _inputs, _targets  = build_graph()
     n = 0
-    p = 0
     with tf.Session(graph=graph) as session:
       # We must initialize all variables before we use them.
+        init = tf.global_variables_initializer()
         init.run()
         print("Initialized")
-        if p+seq_length+1 >= len(data) or n == 0: 
-            p = 0 # go from start of data
-        inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
-        targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+        total_size = batch_size * seq_length
+        inputs = [char_to_ix[ch] for ch in data[:total_size]]
+        targets = [char_to_ix[ch] for ch in data[1:total_size + 1]]
         while True:
-            _, loss = session.run([train_op, loss], feed_dict = {inputs: inputs,
-                    targets: targets})
+            _, loss = session.run([train_op, _loss], feed_dict = {_inputs: inputs,
+                    _targets: targets})
             if n % 100 == 0:
                 print 'iter %d, loss: %f' % (n, loss) # print progress
+            n += 1
 
 if __name__ == '__main__':
     main()
