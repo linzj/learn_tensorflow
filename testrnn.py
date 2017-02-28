eadd4b719eb434cf9e0b78abbf8317fc0c3c0048
linzj <manjian2006@gmail.com>
2017-02-28 08:28:21 +0800

    The sample op fucks the loss.

--- testrnn.py
+++ testrnn.py
@@ -15,7 +15,7 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 hidden_size = 100 # size of hidden layer of neurons
 seq_length = 25 # number of steps to unroll the RNN for
 batch_size = data_size / seq_length
-learning_rate = 1e-1
+learning_rate = 4e-1
 
 graph = tf.Graph()
 def build_graph(keep_prob = 1.0):
@@ -29,7 +29,7 @@ def build_graph(keep_prob = 1.0):
             embedding = tf.get_variable(
                 "embedding", [vocab_size, hidden_size], dtype=tf.float32, initializer=tf.random_normal_initializer(stddev = 1 / math.sqrt(hidden_size)))
             inputs = tf.nn.embedding_lookup(embedding, _inputs)
-        cell = tf.contrib.rnn.BasicLSTMCell(hidden_size)
+        cell = tf.contrib.rnn.BasicLSTMCell(hidden_size, forget_bias=1.0)
         if keep_prob < 1:
             inputs = tf.nn.dropout(inputs, keep_prob)
 
@@ -50,8 +50,7 @@ def build_graph(keep_prob = 1.0):
         losses = tf.nn.softmax_cross_entropy_with_logits(labels=_targets, logits=logits)
         loss = tf.reduce_sum(losses)
         train_op = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
-        sample_op = tf.nn.softmax(logits, 0, name="sample_softmax")
-        sample_op = tf.argmax(sample_op, 1, name="sample_argmax")
+        sample_op = tf.argmax(logits, 1, name="sample_argmax")
         return train_op, loss, r_inputs, r_targets, sample_op
         
 
@@ -66,17 +65,18 @@ def main():
         total_size = batch_size * seq_length
         inputs = [char_to_ix[ch] for ch in data[:total_size]]
         targets = [char_to_ix[ch] for ch in data[1:total_size + 1]]
+        print 'batch_size: %d' % batch_size
         while True:
             _, loss = session.run([train_op, _loss], feed_dict = {_inputs: inputs,
                     _targets: targets})
             if n % 100 == 0:
                 print 'iter %d, loss: %f' % (n, loss) # print progress
-                #start_index = random.randint(20, total_size)
-                #sample_inputs = data[start_index:total_size] + data[0:start_index]
-                #inputs = [char_to_ix[ch] for ch in sample_inputs]
-                #sample_ix = session.run(sample_op, feed_dict = {_inputs: inputs})
-                #txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-                #print '----\n %s \n----' % (txt, )
+                start_index = random.randint(1, batch_size) * seq_length
+                sample_inputs = data[start_index:total_size] + data[0:start_index]
+                inputs = [char_to_ix[ch] for ch in sample_inputs]
+                sample_ix = session.run(sample_op, feed_dict = {_inputs: inputs})
+                txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+                print '----\n %s \n----' % (txt, )
             n += 1
 
 if __name__ == '__main__':
